1a02da942fe2c65baa54f305f4fd3a3098818e75
Rafiq <rafiqzeynalov1331@gmail.com>
2024-01-26 19:32:46 +0400

    first commit

---
+++ payday-ms/src/main/java/com/example/paydayms/PaydayMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.paydayms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaydayMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaydayMsApplication.class, args);
+	}
+
+}
---
+++ payday-ms/src/test/java/com/example/paydayms/PaydayMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paydayms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaydayMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
